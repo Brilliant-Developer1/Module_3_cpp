5117c134dddefa70d6c06b962653754d992e5466
Brilliant-Developer1 <email2obail@gmail.com>
2023-05-25 20:03:10 +0600

    first commit

---
+++ constructor.cpp
@@ -0,0 +1,92 @@
+// #include<bits/stdc++.h>
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <complex>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <strstream>
+#include <tuple>
+#include <typeinfo>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+using namespace std;
+
+class Student
+{
+public:
+    char name[100];
+    int roll;
+    int cls;
+    char section;
+
+    Student(int r, int s, int c, char *n)
+    {
+        roll = r;
+        section = s;
+        cls = c;
+        strcpy(name, n);
+    }
+};
+
+int main()
+{
+
+    Student korim(29, 'A', 7, "Korim");
+    Student rohim(30, 'B', 7, "Rahim");
+
+    cout << rohim.name << endl;
+    cout << rohim.cls << endl;
+    cout << korim.name << endl;
+    cout << korim.section << endl;
+
+    return 0;
+}
